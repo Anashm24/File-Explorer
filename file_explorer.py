923f0cfd2e1f5d85e47952ac34b9c31dd4f4555e
Anashm24 <anashoumaid19@gmail.com>
2024-01-06 18:22:14 +0100

    new

---
+++ file_explorer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import os 
+def file_explorer():
+    dir_stack = []
+    while True:
+        print("current working directory:,", os.getcwd())
+        command = input("Enter command $ ")
+        if command.startswith("cd "):
+            dir_stack.append(os.getcwd())
+            try:
+                os.chdir(command[3:])
+            except FileNotFoundError:
+                print("Directory not found")
+                os.chdir(dir_stack.pop())
+        elif command == "ls":
+            print(os.listdir(os.getcwd()))
+        elif command.startswith("mkdir "):
+            os.mkdir(command[6:])
+        elif command.startswith("rmdir "):
+            os.rmdir(command[6:])
+        elif command == "back":
+            if dir_stack:
+                os.chdir(dir_stack.pop())
+            else:
+                print("No previous directory")
+        elif command == "exit":
+            break
+        else:
+            print("Invalid command")
+
+file_explorer()
